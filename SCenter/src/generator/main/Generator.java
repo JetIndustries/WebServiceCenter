fe27bb2748292829312bfc20ce1cf87bb83b8c3d
Anrew Petrishin <petrishin.andrew@gmail.com>
2016-02-12 21:20:01 +0200

    new Test

---
+++ SCenter/src/generator/main/Generator.java
@@ -0,0 +1,91 @@
+package generator.main;
+
+import java.util.Random;
+
+public class Generator {
+	private static String[] names = { "Jacob", "Tyler", "Lauren", "Sophia", "Alexander", "Alyssa", "David", "Grace",
+			"Nicholas", "Sarah", "Ryan", "Alexis", "Anthony", "Elizabeth", "Christopher", "Joseph", "Samantha",
+			"Samantha", "Ashley", "Daniel", "Isabella", "Andrew", "Abigail", "Hannah", "Ethan", "Olivia", "Matthew",
+			"Madison", "Joshua", "Emma", "Michael", "Emily" };
+	
+	private static String[] last_name = {	"Mcadoo", "Seckman", "Shut", "Del", "Solarz", "Sabal", "Mark", "Dulan", "Kasper", 
+			"Melodi", "Alosta", "Kenneth", "Schnabl", "Lamar", "Franken", "Mittie", "Tommy", "Young", "Aschim", "Milissa", 
+			"Middlebrooks", "Marge", "Lavanchy", "Roman", "Halaby", "Rusaw", "Felicia", "Geerdes", "Soules", "Christie", "Leflore", 
+			"Wendy", "Steffel", "Urbaez", "Yong", "Pattie"};
+	
+	private static Random gen = new Random();
+	private static String[] mails = {"@mail.ru", "@gmail.com", "ukr.net", "@hotline.com", "@telnet.com", "@gov.co.il", "@nesses.com"};
+
+	
+	private static String[] positionJob = {"manager", "technic", "support", "director"};
+	private static String[] product = {"LG", "Samsung","WD","MAG","Sigma","Optiquest", "Nec", "Logitech", "A4","MicroLab", "Acer", "Asus", "HP"};
+	private static String[] manufacturing = {"Monitor", "HDD", "Mouse", "Keyboard", "Laptop", "Power", "CDRom", "Sound"};
+	private static String[] streets = { "Lenina", "Rothschild", "Yerushalayim", "Ben Gurion", "Allenby", "Dizengoff",
+			"HaArbaa", "HaMasger", "HaYarkon", "Ibn Gabirol" };
+	private static String[] cities = { "Haifa", "Tel Aviv", "Eilat", "Jerusalem", "Beer Sheva", "Rehovot" };
+	
+	
+
+	public static String genPhoneNumber(int countDigit){
+		StringBuffer res = new StringBuffer("0");
+		for(int i=0; i<countDigit; i++){
+			res.append(genInt(9));
+		}
+		return res.toString();
+	}
+	
+	public static String genSerialNumber(int countDigit, String name){
+		StringBuffer res = new StringBuffer("");
+		res.append(name.charAt(0));
+		for(int i=0; i<countDigit; i++){
+			res.append(genIntChar(name));
+		}
+		return res.toString();
+	}
+	
+	public static String genName(){
+		return genFromArray(names);
+	}
+	
+	public static String genLastName(){
+		return genFromArray(last_name);
+	}
+	
+	public static String genPosJob(){
+		return genFromArray(positionJob);
+	}	
+	
+	public static String genProduct(){
+		return genFromArray(product);
+	}
+
+	public static String genEmail(String name, String name_last){
+		return name.toLowerCase() + "_" + name_last.toLowerCase() +  genFromArray(mails);
+	}
+	
+	public static String genAdress(){
+		return genFromArray(cities) + ", " + genFromArray(streets) + " " + genInt(159);
+	}	
+	
+	private static String genIntChar(String name) {
+		char c[] = name.toCharArray();
+		int num = genInt(8 + c.length);
+		return num>9? Character.toString(c[num - 9]).toUpperCase() : Integer.toString(num);
+	}
+
+	public static int genInt(int max) {
+		return max > 0 ? 1 + gen.nextInt(max) : 1 + gen.nextInt(255);
+	}
+	
+	private static String genFromArray(String[] array){
+		return array[gen.nextInt(array.length)];
+	}
+
+	public static String genCities() {
+		return genFromArray(cities);
+	}
+	
+	public static String genManufactur() {
+		return genFromArray(manufacturing);
+	}
+}
---
+++ SCenter/src/generator/main/GeneratorTables.java
@@ -0,0 +1,83 @@
+package generator.main;
+
+import java.util.*;
+
+import service_center.dao.Client;
+import service_center.dao.ComplexityRepair;
+import service_center.dao.Position;
+import service_center.dao.Product;
+import service_center.dao.Shop;
+import service_center.dao.User;
+
+public class GeneratorTables {
+
+	public List<Client> genClients(int count) {
+		String name, lname, phone, email, adress;
+		List<Client> res = new LinkedList<Client>();
+		for (int i = 0; i < count; i++) {
+			name = Generator.genName();
+			lname = Generator.genLastName();
+			phone = Generator.genPhoneNumber(10);
+			email = Generator.genEmail(name, lname);
+			adress = Generator.genAdress();
+			res.add(new Client(name, lname, phone, email, adress));
+		}
+		return res;
+	}
+	
+	public List<Position> genPosJobs(int count) {
+		List<Position> res = new LinkedList<Position>();
+		for (int i = 0; i < count; i++) {
+			int access = Generator.genInt(9);
+			String job = Generator.genPosJob();
+			res.add(new Position(access, job));
+		}
+		return res;
+	}
+	
+	public List<Shop> genShops(int count) {
+		List<Shop> res = new LinkedList<Shop>();
+		for (int i = 0; i < count; i++) {
+			String name = Generator.genCities();
+			String code = Generator.genSerialNumber(3, name);
+			res.add(new Shop(name, code));
+		}
+		return res;
+	}
+	
+	public List<ComplexityRepair> genCRepairs(int count) {
+		List<ComplexityRepair> res = new LinkedList<ComplexityRepair>();
+		for (int i = 0; i < count; i++) {
+			String name = Generator.genManufactur();
+			int time = Generator.genInt(9) * 10;
+			res.add(new ComplexityRepair(name, time));
+		}
+		return res;
+	}
+	
+	public List<Product> genProducts(int count) {
+		List<Product> res = new LinkedList<Product>();
+		for (int i = 0; i < count; i++) {
+			String name = Generator.genProduct();
+			String sn = Generator.genSerialNumber(10, name);
+			int waranty = Generator.genInt(3);
+			String mn = Generator.genManufactur();
+			String model = Generator.genSerialNumber(5, name);
+			res.add(new Product(name, sn, waranty, mn, model));
+		}
+		return res;
+	}
+	
+	public List<User> genUsers(int count) {
+		List<User> res = new LinkedList<User>();
+		for (int i = 0; i < count; i++) {
+			String name = Generator.genName();
+			String lname = Generator.genLastName();
+			String phone = Generator.genPhoneNumber(10);
+			String login = Generator.genName().substring(1, 3).toLowerCase() + Generator.genPhoneNumber(4) + Generator.genSerialNumber(3, lname);
+			String password = "admin";
+			res.add(new User(login, password, name, lname, phone));
+		}
+		return res;
+	}
+}
